e8f262af04b838b244a5c154f4612859512e9c7b
Dave Blacksmith <daveblacksmith@hotmail.com>
2020-04-21 20:18:55 +0200

    Revert "Puntuación terminada, a falta de añadirla en la vista."
    This reverts commit 2c210602eb120b0bddec806bbbe9c21f9f2f8d51.

--- src/logic/Puntuacion.java
+++ src/logic/Puntuacion.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package logic;
 
 /**
@@ -5,34 +10,5 @@ package logic;
  * @author davit
  */
 public class Puntuacion {
-    private int puntos;
     
-    private final int CARTAS_CORRECTAS = 10;
-    private final int CARTAS_INCORRECTAS = 5;
-    
-
-    public Puntuacion(int puntos) {
-        this.puntos = puntos;
-    }
-
-    public int getPuntos() {
-        return puntos;
-    }
-
-    public void setPuntos(int puntuacionFinal) {
-        this.puntos = puntos;
-    }
-    
-    public int sumarPuntos(){
-        return puntos += CARTAS_CORRECTAS;
-    }
-    
-     public int restarPuntos(){
-        return puntos -= CARTAS_INCORRECTAS;
-    }
-
-    @Override
-    public String toString() {
-        return "Puntuacion{" + "puntos=" + puntos + '}';
-    }
 }
